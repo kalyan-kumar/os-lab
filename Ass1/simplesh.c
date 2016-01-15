2fd1943c6a6650d694c77718241283c8062b21e5
Kalyan Kumar <kalyankumar.garapaty@gmail.com>
2016-01-15 17:49:40 +0530

    Add env, mkdir functionalities and indent code.
    Signed-off-by: Kalyan Kumar <kalyankumar.garapaty@gmail.com>

--- Ass1/simplesh.c
+++ Ass1/simplesh.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>           // Necessary for stat used before mkdir
+#include <sys/types.h>          // Necessary for mkdir
+#include <unistd>               // Necessary for rmdir
 
 
 int getcw(char cwd[])
@@ -12,86 +15,71 @@ int getcw(char cwd[])
        perror("getcwd() error");
    return 0;
 }
-int main()
+
+int main(int argc, char **argv, char **envp)
 {
-  char cwd[1024];
+    char cwd[1024];
     char write[100];
     while(1)
     { 
-      int i=1;
-      getcwd(cwd,sizeof(cwd));
-      fprintf(stdout, "%s >", cwd);
-    fgets (write, 100, stdin);
-    char* tokens[100];
-    tokens[0]=(char*)malloc(100*sizeof(char));
-    char *q=strtok(write,"\n");
-    char *p =tokens[0]= strtok(q, " ");
+        int i=1;
+        getcwd(cwd,sizeof(cwd));
+        fprintf(stdout, "%s >", cwd);
+        fgets (write, 100, stdin);
+        char* tokens[100];
+        tokens[0]=(char*)malloc(100*sizeof(char));
+        char *q=strtok(write,"\n");
+        char *p =tokens[0]= strtok(q, " ");
 
-    while(p != NULL)
-    { 
-      tokens[i]=(char*)malloc(100*sizeof(char));
-        p =tokens[i]=strtok(NULL, " ");
-        i++;
-      }
+        while(p != NULL)
+        {
+            tokens[i]=(char*)malloc(100*sizeof(char));
+            p =tokens[i]=strtok(NULL, " ");
+            i++;
+        }
       
-      if(!strcmp("cd",tokens[0]))
-      { 
-        
-        if(i!=1)
-        { 
-          if(!strcmp(tokens[1][0],"\"")
-          {
-            int len=strlen(tokens[1]);
-            if(!strcmp(tokens[1][len-1],"\"")
-            {
-              
+        if(!strcmp("cd",tokens[0]))
+        {
+            if(i!=1)
+            { 
+                if(!strcmp(tokens[1][0],"\""))
+                {
+                    int len=strlen(tokens[1]);
+                    if(!strcmp(tokens[1][len-1],"\"")){}
+                }
+                int status=chdir(tokens[1]);
+                if(status==-1)
+                    perror("cd ");
             }
-          }
-          int status=chdir(tokens[1]);
-          if(status==-1)
-            perror("cd ");
+            else
+                chdir(getenv("HOME"));
+        }
+        else if(!strcmp("pwd",tokens[0]))
+        {
+            getcwd(cwd,sizeof(cwd));
+            fprintf(stdout, "%s\n", cwd);
+        }
+        else if(!strcmp("clear",tokens[0]))
+        {
+            printf("\e[2J\e[H");
+        }
+        else if(!strcmp("env", tokens[0]))
+        {
+            char **env;
+            for(env=envp;*env!=0;env++)
+                printf("%s\n", *env);
+        }
+        else if(!strcmp("mkdir", tokens[0]))
+        {
+            struct stat st = {0};
+            if(stat(tokens[1], &st) == -1)
+                mkdir(tokens[1], 0700);
+            else
+                printf("mkdir: cannot create directory ‘%s’: File exists\n", tokens[1]);
+        }
+        else if(!strcmp("rmdir", tokens[0]))
+        {
 
         }
-        else
-          chdir(getenv("HOME"));
-      }
-      if(!strcmp("pwd",tokens[0]))
-      {
-        getcwd(cwd,sizeof(cwd));
-        fprintf(stdout, "%s\n", cwd);
-      }
-      if(!strcmp("clear",tokens[0]))
-      {
-        printf("\e[2J\e[H");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      }
-      
-        
-       
-
-
-  // }
-      }
-
-
-   
-
+    }
 }
